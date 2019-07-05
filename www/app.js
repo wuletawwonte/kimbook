7f8e092f3048e56d4ae8296d29942d349d1bfeb4
Wuletaw <wuletawwonte@yahooo.com>
2019-07-05 22:47:50 +0300

    sidemenu not working

--- www/app.js
+++ www/app.js
@@ -2,29 +2,22 @@
 
 var app = angular.module('kimBook', ['onsen', 'ngRoute', 'ngSQLite']);
 
-app
-	.constant('DB_CONFIG', {
-        loan: {
-            id: 'key',
-            borrower: { type: 'text', null: false },
-            loan_amount: { type: 'text' }
-        }
-    })
- 
-    .run(function ($SQLite) {
-        $SQLite.dbConfig({
-            name: 'kimbook-db',
-            description: 'Bidir Registration Database',
-            version: '1.0'
-        });
-    })
- 
-    .run(function ($SQLite, DB_CONFIG) {
-        $SQLite.init(function (init) {
-            angular.forEach(DB_CONFIG, function (config, name) {
-                init.step();
-                $SQLite.createTable(name, config).then(init.done);
-            });
-            init.finish();
-        });
-    });
+app.config(function($routeProvider) { 
+	$routeProvider
+	.when('/', {
+		templateUrl: './templates/home.html',
+		controller: 'homeCtrl'
+	}).when('/election', {
+		templateUrl: './templates/election.html',
+		controller: 'electionCtrl'
+	}).otherwise({
+		template: '404'
+	})
+});
+
+
+app.controller('homeCtrl', function() {
+
+	mySplitter.content.load('./templates/home.html');
+
+})
