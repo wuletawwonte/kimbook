3e6289bd40b7f6a52dd4c14e7ce01ba9c402fea9
Wuletaw <wuletawwonte@yahooo.com>
2019-07-05 17:07:32 +0300

    res folder added

--- www/app.js
+++ www/app.js
@@ -1,3 +1,30 @@
 
 
-var app = angular.module('kimBook', ['onsen', 'ngRoute']);
+var app = angular.module('kimBook', ['onsen', 'ngRoute', 'ngSQLite']);
+
+app
+	.constant('DB_CONFIG', {
+        loan: {
+            id: 'key',
+            borrower: { type: 'text', null: false },
+            loan_amount: { type: 'text' }
+        }
+    })
+ 
+    .run(function ($SQLite) {
+        $SQLite.dbConfig({
+            name: 'kimbook-db',
+            description: 'Bidir Registration Database',
+            version: '1.0'
+        });
+    })
+ 
+    .run(function ($SQLite, DB_CONFIG) {
+        $SQLite.init(function (init) {
+            angular.forEach(DB_CONFIG, function (config, name) {
+                init.step();
+                $SQLite.createTable(name, config).then(init.done);
+            });
+            init.finish();
+        });
+    });
