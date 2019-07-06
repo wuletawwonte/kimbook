039a13f4fcc3d09725fab5568be24dc7dba31309
Wuletaw <wuletawwonte@yahooo.com>
2019-07-06 10:49:04 +0300

    to be pushed to gitlab

--- www/app.js
+++ www/app.js
@@ -1,23 +1,9 @@
 
 
-var app = angular.module('kimBook', ['onsen', 'ngRoute', 'ngSQLite']);
-
-app.config(function($routeProvider) { 
-	$routeProvider
-	.when('/', {
-		templateUrl: './templates/home.html',
-		controller: 'homeCtrl'
-	}).when('/election', {
-		templateUrl: './templates/election.html',
-		controller: 'electionCtrl'
-	}).otherwise({
-		template: '404'
-	})
-});
+var app = angular.module('kimBook', []);
 
 
 app.controller('homeCtrl', function() {
-
-	mySplitter.content.load('./templates/home.html');
+	// mySplitter.content.load('./templates/home.html');
 
 })
